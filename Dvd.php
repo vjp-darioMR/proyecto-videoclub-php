f36dfa880e6c57cd4dc6ae042d988736fc28716a
Yago Garcia Alonso <ygarciaa03@instituto.extremadura.es>
2025-10-17 15:55:52 +0200

    Ejercicio_322

---
+++ Dvd.php
@@ -0,0 +1,20 @@
+<?php
+require_once "Soporte.php";
+
+class Dvd extends Soporte {
+    private $idiomas;
+    private $formatoPantalla;
+
+    public function __construct($titulo, $numero, $precio, $idiomas, $formatoPantalla) {
+        parent::__construct($titulo, $numero, $precio);
+        $this->idiomas = $idiomas;
+        $this->formatoPantalla = $formatoPantalla;
+    }
+
+    public function muestraResumen() {
+        parent::muestraResumen();
+        echo "<br>Idiomas: " . $this->idiomas;
+        echo "<br>Formato: " . $this->formatoPantalla;
+    }
+}
+?>
--- inicio.php
+++ inicio.php
@@ -1,14 +1,19 @@
 <?php
 include "Soporte.php";
 include "CintaVideo.php";
+include "Dvd.php";
 
 $Soporte1 = new Soporte("Tenet", 22, 3);
 echo "<h3>" . $Soporte1->getTitulo() . "</h3>"; 
 $Soporte1->muestraResumen();
 
 
-
 $SmiCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 127);
 echo "<h3>" . $SmiCinta->getTitulo() . "</h3>"; 
 $SmiCinta->muestraResumen();
+
+
+$SmiDvd = new Dvd("Origen", 24, 15, "Español, Ingles, Frances", "16:9");
+echo "<h3>" . $SmiDvd->getTitulo() . "</h3>";
+$SmiDvd->muestraResumen();
 ?>
